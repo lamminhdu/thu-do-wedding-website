da5e8436c61dc08a59485debc0335fe94616f966
Bùi Kiệm <buivantrung1105@gmail.com>
2023-12-10 07:56:36 +0000

    feat(devtool): Install biome, husky, lint-staged, and commitlint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function HomePage() {
-  return (
-   <main>
-    <h1>Homepage Test</h1> 
-    </main>
-  );
+	return (
+		<main>
+			<h1>Homepage Test</h1>
+			<p>test</p>
+		</main>
+	);
 }
